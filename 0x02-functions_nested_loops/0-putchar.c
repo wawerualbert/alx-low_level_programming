6e00eaf83b60a5de325a44f33388762b3c002bf0
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2022-03-15 21:51:39 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - Print out reverse alphabet.
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+​#​include​ ​"​stdio.h​" 
+ ​/​ 
+ ​ * main - Prints "_putchar" followed by a new line. 
+ ​ * 
+ ​ * Return: Always 0 (Success) 
+ ​ * 
+ ​ ​*/ 
+  
+ ​int​ ​main​(​void​) 
+ ​{ 
+ ​        ​int​ i; 
+ ​        ​char​ p[] = ​"​_putchar​"​; 
+  
+ ​        ​for​ (i = ​0​; i < ​8​; i++) 
+ ​        { 
+ ​                ​putchar​(p[i]); 
+ ​        } 
+ ​        ​putchar​(​'​\n​'​); 
+ ​return​ (​0​); 
+ ​}
