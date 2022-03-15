30da2d44211e4fceee03863f11e64fcae1de5065
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2022-03-15 21:52:31 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 ​#​include​ ​"​stdio.h​" 
- ​/​** 
+ ​/** 
  ​ * main - Prints "_putchar" followed by a new line. 
  ​ * 
  ​ * Return: Always 0 (Success) 
  ​ * 
- ​ ​*/ 
+ ​ */ 
   
  ​int​ ​main​(​void​) 
  ​{ 
