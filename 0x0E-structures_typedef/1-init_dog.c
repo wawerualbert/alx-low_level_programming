aa36c1a048015f5c70f86f0a64c483302f080894
wawerualbert <wawerualbert58@gmail.com>
2022-04-12 01:12:52 +0300

    Structs and typedefs in c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type `struct dog`
+ * @d: struct dog
+ * @name: string for name
+ * @age : integer for age
+ * @owner: string for owners
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
+
+
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free dog - free dog memory
+ * @d: dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+
+}
+
+
+
