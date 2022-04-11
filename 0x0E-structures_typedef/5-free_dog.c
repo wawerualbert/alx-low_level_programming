96968988d8970c4dbd51e8555de454d70f61858e
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2022-04-12 01:16:36 +0300

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,13 +5,12 @@
  * free dog - free dog memory
  * @d: dog
  */
-
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+if (d == NULL)
+	return;
 
+free(d->owner);
+free(d->name);
+free(d);
 }
